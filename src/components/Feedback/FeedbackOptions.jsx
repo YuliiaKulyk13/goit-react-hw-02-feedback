ed4d6b99204a1f7deddfd45ada507b20567176db
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-03-26 05:46:08 -0400

    done

---
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => {
+        return (
+          <button
+            type="button"
+            key={option}
+            onClick={() => {
+              onLeaveFeedback(option);
+            }}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
